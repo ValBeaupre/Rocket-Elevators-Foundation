d2fee696ccf3da9e981ddbbc2cd020e835c2109f
ValBeaupre <valbeaupre@hotmail.com>
2018-12-07 14:01:08 -0500

    host domain url changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
     def welcome_email
     @user = params[:user]
-    @url  = 'http://rocketelevators.ca/login'
+    @url  = 'http://valbeaupre.com/login'
     mail(to: @user.email, subject: 'Welcome to Rocket Elevators')
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'rocketelevators.ca' }
+  config.action_mailer.default_url_options = { host: 'valbeaupre.com' }
 
   ActionMailer::Base.smtp_settings = {
   :address                => "smtp.gmail.com",
